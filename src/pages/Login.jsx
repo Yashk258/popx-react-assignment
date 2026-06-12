eb15b236eb8dea32a7dfce05a24558a31713e8d6
Yash Kumar <yashkumar258a@gmail.com>
2026-06-12 19:43:22 +0530

    Added validation and completed assignment

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
   const navigate = useNavigate();
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const isFormValid =
+    email.trim() !== "" &&
+    password.trim() !== "";
+
+  const handleLogin = () => {
+    if (isFormValid) {
+      navigate("/profile");
+    }
+  };
+
   return (
     <div className="app-container">
       <div className="mobile-frame login-frame">
-        <h1>Signin to your PopX account</h1>
+        <h1>
+          Signin to your
+          <br />
+          PopX account
+        </h1>
 
         <p>
           Lorem ipsum dolor sit amet,
@@ -16,23 +34,38 @@ function Login() {
 
         <div className="input-group">
           <label>Email Address</label>
+
           <input
             type="email"
             placeholder="Enter email address"
+            value={email}
+            onChange={(e) =>
+              setEmail(e.target.value)
+            }
           />
         </div>
 
         <div className="input-group">
           <label>Password</label>
+
           <input
             type="password"
             placeholder="Enter password"
+            value={password}
+            onChange={(e) =>
+              setPassword(e.target.value)
+            }
           />
         </div>
 
         <button
-          className="login-btn"
-          onClick={() => navigate("/profile")}
+          className={`login-btn ${
+            isFormValid
+              ? "login-btn-active"
+              : ""
+          }`}
+          disabled={!isFormValid}
+          onClick={handleLogin}
         >
           Login
         </button>
